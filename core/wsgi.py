b84a5f1a96122ea538fc99b84b6f38572e43d00f
daohoang1105 <79619910+daohoang1105@users.noreply.github.com>
2022-07-08 15:39:36 +0700

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,8 @@
+
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django.core")
+
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
